43e44f8a20a0c2c1f3455c5ceb47a3e7ddb815cc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-28 13:21:31 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -103,27 +103,30 @@ function shoot(e) {
   let laserId
   let currentLaserIndex = currentShooterIndex
   function moveLaser() {
-    squares[currentLaserIndex].classList.remove('laser')
-    currentLaserIndex -= width
-    squares[currentLaserIndex].classList.add('laser')
-
-    if (squares[currentLaserIndex].classList.contains('invader')) {
-      squares[currentLaserIndex].classList.remove('laser')
-      squares[currentLaserIndex].classList.remove('invader')
-      squares[currentLaserIndex].classList.add('boom')
-
-      setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
-      clearInterval(laserId)
-
-      const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
-      aliensRemoved.push(alienRemoved)
-      results++
-      resultsDisplay.innerHTML = results
-      console.log(aliensRemoved)
-
+ 
+    if (currentLaserIndex > 15) {
+        currentLaserIndex -= width
+        squares[currentLaserIndex].classList.add('laser')
+        setTimeout(()=> squares[currentLaserIndex].classList.remove('laser'), 35)
+    
+        if (squares[currentLaserIndex].classList.contains('invader')) {
+          squares[currentLaserIndex].classList.remove('laser')
+          squares[currentLaserIndex].classList.remove('invader')
+          squares[currentLaserIndex].classList.add('boom')
+    
+          setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+          clearInterval(laserId)
+    
+          const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+          aliensRemoved.push(alienRemoved)
+          results++
+        } 
+          resultsDisplay.innerHTML = results
+        }
+    
     }
-
-  }
+    
+  
   switch(e.key) {
     case 'ArrowUp':
       laserId = setInterval(moveLaser, 100)
