7f21304d6c628eb8bf04347a7046214b5f602ecd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-28 11:19:20 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const grid = document.querySelector('.grid');
 let currentShooterIndex = 202;
 let width = 15;
+let direction = 1;
+let invadersId;
+let goingRight = true;
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div');
@@ -50,8 +53,33 @@ function moveInvaders() {
     const leftEdge = alienInvaders[0] % width === 0;
     const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1;
     remove();
+
+    if (rightEdge && goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width + 1;
+            direction = - 1;
+            goingRight = false;
+        }
+    }
+
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width - 1;
+            direction = 1;
+            goingRight = true;
+        }
+    }
+
     for (let i = 0; i < alienInvaders.length; i++) {
-        alienInvaders[i] += 1;
+        alienInvaders[i] += direction;
     };
+
     draw();
+
+    if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+        console.log('game over');
+        clearInterval(invadersId);
+    }
 }
+
+invadersId = setInterval(moveInvaders, 500)
